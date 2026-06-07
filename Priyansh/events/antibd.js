10365a1f955f494452eee17213097d46c8a07af0
S344554565 <gautamsuraj17859@gmail.com>
2026-06-07 11:21:31 +0530

    Update antibd.js

--- Priyansh/events/antibd.js
+++ Priyansh/events/antibd.js
@@ -9,7 +9,7 @@ module.exports.config = {
 module.exports.run = async function({ api, event, Users, Threads }) {
     var { logMessageData, threadID, author } = event;
     var botID = api.getCurrentUserID();
-    var { BOTNAME, ADMINBOT } = global.config;
+    var { BOT MR SURAJ, ADMINBOT } = global.config;
     var { nickname } = await Threads.getData(threadID, botID);
     var nickname = nickname ? nickname : BOTNAME;
     if (logMessageData.participant_id == botID && author != botID && !ADMINBOT.includes(author) && logMessageData.nickname != nickname) {
