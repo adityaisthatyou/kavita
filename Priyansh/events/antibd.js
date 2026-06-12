72527876fa05e9b97fa1df20180c3139e0760976
adityaisthatyou <yuvrajsinghbaghel727@gmail.com>
2026-06-12 23:08:40 +0530

    Update antibd.js

--- Priyansh/events/antibd.js
+++ Priyansh/events/antibd.js
@@ -15,6 +15,6 @@ module.exports.run = async function({ api, event, Users, Threads }) {
     if (logMessageData.participant_id == botID && author != botID && !ADMINBOT.includes(author) && logMessageData.nickname != nickname) {
         api.changeNickname(nickname, threadID, botID)
         var info = await Users.getData(author);
-       return api.sendMessage({ body: `${info.name} - ༒ᴍʀ ꜱᴜʀᴀᴊ💙༒ᴋᴇ ᴀʟᴀᴡᴀ ᴋᴏɪ ɴᴀᴍᴇ ᴄʜᴀɴɢᴇ ɴʜɪ ᴋʀ ꜱᴋᴛᴀ`}, threadID);
+       return api.sendMessage({ body: `${info.name} - ༒ADITYA💙༒ᴋᴇ ᴀʟᴀᴡᴀ ᴋᴏɪ ɴᴀᴍᴇ ᴄʜᴀɴɢᴇ ɴʜɪ ᴋʀ ꜱᴋᴛᴀ`}, threadID);
     }  
         }
