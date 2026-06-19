e6a863e48456b4fe87b4a6378882dc729d2de1d5
adityaisthatyou <yuvrajsinghbaghel727@gmail.com>
2026-06-19 17:46:10 +0530

    Update permission message for Offbot command

--- Priyansh/commands/Offbot.js
+++ Priyansh/commands/Offbot.js
@@ -9,6 +9,6 @@ module.exports.config = {
         };
 module.exports.run = ({event, api}) =>{
     const permission = ["61576615631528", "61576615631528"];
-  	if (!permission.includes(event.senderID)) return api.sendMessage("[ ERR ] You don't have permission to use this command, This Command Only For Shaan", event.threadID, event.messageID);
+  	if (!permission.includes(event.senderID)) return api.sendMessage("[ ERR ] You don't have permission to use this command, This Command Only For aditya", event.threadID, event.messageID);
   api.sendMessage(`[ OK ] ${global.config.BOTNAME} Bot are now turned off.`,event.threadID, () =>process.exit(0))
 }
